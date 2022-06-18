73055c88bdd674ba30cdd93d891892f6bead5bcd
Andreslis19 <gaml_16@hotmail.com>
2022-06-18 17:19:29 -0500

    cambios

--- p1.py
+++ p1.py
@@ -1,16 +1,21 @@
-print("hola mundo")
+"""print("hola mundo")
 mensaje_1= "HOLA COMO ESTAS"
 print(mensaje_1)
-
 nombre=input("cual es tu nombre?")
-if nombre=="juan":(
-
 print("mucho gusto", nombre, " me llamo andres")
-)
-else:(
-    print("carlos")
-)
-cedula=1054557477
-print((cedula))
+
+resultado=(((3+2)/(2*5))**2)
+print(resultado)
+"""
+
+from xml.dom.expatbuilder import parseString
+
+
+num_horas= float(input("cuantas horas labora: "))
+costo_horas=float(input("cuanto cuesta la hora laboral: "))
+resultado=(num_horas*costo_horas)
+print(resultado)
+
+
 
 
