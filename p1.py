e071cc6ec34ce155bb0db8eef73bf5f0ee5e27bc
Andreslis19 <gaml_16@hotmail.com>
2022-06-18 17:20:00 -0500

    confirmar

--- p1.py
+++ p1.py
@@ -3,18 +3,31 @@ mensaje_1= "HOLA COMO ESTAS"
 print(mensaje_1)
 nombre=input("cual es tu nombre?")
 print("mucho gusto", nombre, " me llamo andres")
-
 resultado=(((3+2)/(2*5))**2)
 print(resultado)
 """
 
-from xml.dom.expatbuilder import parseString
 
-
-num_horas= float(input("cuantas horas labora: "))
+"""num_horas= float(input("cuantas horas labora: "))
 costo_horas=float(input("cuanto cuesta la hora laboral: "))
 resultado=(num_horas*costo_horas)
-print(resultado)
+print(resultado)"""
+
+"""numero_entero=int(input("escriba un numero cualquiera: "))
+suma=int((numero_entero*(numero_entero+1))/2)
+print("la suma de los primeros numeros enteros desde 1 hasta",numero_entero,"es ",suma)"""
+
+"""nombre=input("ingrese su nombre: ")
+numero=int(input("ingrese un numero entero: "))
+contador=0
+while(contador<numero):
+    print(nombre)
+    contador=contador+1"""
+
+
+def suma(a,b):
+    resultado=a+b
+    return resultado
 
 
 
---
+++ p2.py
@@ -0,0 +1,6 @@
+import p1
+
+a1=int(input("numero1: "))
+b1=int(input("numero2: "))
+
+print("la suma de los dos numeros es ", p1.suma(a1,b1))
