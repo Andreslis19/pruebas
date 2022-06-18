6d1b0c598a06bed28142aa1bf35cdc65eb0051cf
Andreslis19 <gaml_16@hotmail.com>
2022-06-18 15:12:27 -0500

    modificaciones

--- p1.py
+++ p1.py
@@ -1,2 +1,16 @@
 print("hola mundo")
-print("hola papa")
+mensaje_1= "HOLA COMO ESTAS"
+print(mensaje_1)
+
+nombre=input("cual es tu nombre?")
+if nombre=="juan":(
+
+print("mucho gusto", nombre, " me llamo andres")
+)
+else:(
+    print("carlos")
+)
+cedula=1054557477
+print((cedula))
+
+
